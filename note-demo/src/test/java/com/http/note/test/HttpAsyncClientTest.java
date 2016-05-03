e2f8a3f2b151979166f15ef22a0ea42b0c33ce32
Administrator <Administrator@SKY-20151109TXO>
2016-05-03 09:14:04 +0800

    test demo

--- note-demo/src/test/java/com/http/note/test/HttpAsyncClientTest.java
+++ note-demo/src/test/java/com/http/note/test/HttpAsyncClientTest.java
@@ -1,4 +1,4 @@
-package com.http.test;
+package com.http.note.test;
 
 import java.util.concurrent.CountDownLatch;
 
@@ -14,6 +14,8 @@ import org.apache.http.nio.reactor.ConnectingIOReactor;
 import org.apache.http.nio.reactor.IOReactorException;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
+
+import com.http.test.ChuanglanSMS;
 
 public class HttpAsyncClientTest {
 
